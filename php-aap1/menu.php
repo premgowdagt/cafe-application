535724053c18afb5ac1cdce30f4f17e76e8bdee1
premgowdagt <gtpremgowda@gmail.com>
2024-12-25 13:16:06 +0530

    Create menu.php

---
+++ php-aap1/menu.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h1>Menu</h1>";
+echo "<ul>";
+echo "<li>Coffee - $2.50</li>";
+echo "<li>Tea - $2.00</li>";
+echo "<li>Sandwich - $5.00</li>";
+echo "<li>Pastry - $3.50</li>";
+echo "</ul>";
+?>
