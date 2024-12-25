7126a396572912e90630f5d436dad80625c829b2
premgowdagt <gtpremgowda@gmail.com>
2024-12-25 13:17:27 +0530

    Update index.php

--- php-aap1/index.php
+++ php-aap1/index.php
@@ -1,6 +1,5 @@
 <?php
 echo "<h1>Welcome to Cafe</h1>";
 echo "<p><a href='menu.php'>View Menu</a></p>";
-echo "<p><a href='product.php'>Products</a></p>";
 echo "<p><a href='order.php'>Place an Order</a></p>";
 ?>
